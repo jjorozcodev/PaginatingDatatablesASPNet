b28ee5153f58a9fdda5d7cdc9c3fc70ce03a5c55
JJ <jj.orozcolopez.13@gmail.com>
2019-08-26 11:37:00 -0600

    Bootstrap updated, EF context, Controller and View created.
    Bootstrap updated to 4.3.1, JQuery updated to 3.2, Created Controller and View for Empleado.

---
+++ PaginatingDatatablesMVC/Controllers/EmpleadoController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace PaginatingDatatablesMVC.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        // GET: Empleado
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
