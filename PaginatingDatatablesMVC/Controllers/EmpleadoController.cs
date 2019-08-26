f5d3a2efcf9cc864d7b182667ad5fc1b0665d3bf
JJ <jj.orozcolopez.13@gmail.com>
2019-08-26 14:45:10 -0600

    Example finished: Paginating and filtering data.
    Update methods to implement paginating from datatables, filtering and sorting data result. Example tested with 50,000 entries.

--- PaginatingDatatablesMVC/Controllers/EmpleadoController.cs
+++ PaginatingDatatablesMVC/Controllers/EmpleadoController.cs
@@ -2,6 +2,8 @@
 using PaginatingDatatablesMVC.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Dynamic;
+using System;
 
 namespace PaginatingDatatablesMVC.Controllers
 {
@@ -16,12 +18,35 @@ namespace PaginatingDatatablesMVC.Controllers
         [HttpPost]
         public ActionResult GetList()
         {
+            // Server side parameter
+            int start = Convert.ToInt32(Request["start"]);
+            int length = Convert.ToInt32(Request["length"]);
+            string searchValue = Request["search[value]"];
+            string sortColumnName = Request["columns[" + Request["order[0][column]"] + "][name]"];
+            string sortDirection = Request["order[0][dir]"];
+
             List<Empleado> empList = new List<Empleado>();
             using(DBModelConnection db = new DBModelConnection())
             {
-                empList = db.Empleados.Take(10).ToList();
+                empList = db.Empleados.ToList();
+                int totalRows = empList.Count();
+
+                //filter
+                if (!string.IsNullOrEmpty(searchValue))
+                {
+                    empList = empList.Where(e => e.Nombre.ToLower().Contains(searchValue.ToLower()) || e.Apellido.ToLower().Contains(searchValue.ToLower()) || e.Documento.ToLower().Contains(searchValue.ToLower())).ToList();
+                }
+                int totalRowsFiltered = empList.Count();
+
+                //sorting
+                empList = empList.OrderBy(sortColumnName + " " + sortDirection).ToList();
+
+                //paging
+                empList = empList.Skip(start).Take(length).ToList();
+
+                return Json(new { data = empList, draw = Request["draw"], recordsTotal = totalRows, recordsFiltered = totalRowsFiltered }, JsonRequestBehavior.AllowGet);
             }
-            return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
+           
         }
     }
 }
